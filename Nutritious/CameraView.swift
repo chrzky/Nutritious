5145725c969709512944cf1c43d5d573d6ddd413
Christian Fruehauf <christian.fruehauf@tum.de>
2016-03-11 17:03:31 +0100

    first release

---
+++ Nutritious/CameraView.swift
@@ -0,0 +1,237 @@
+//
+//  FirstViewController.swift
+//  Nutritious
+//
+//  Created by Christian Frühauf on 11/03/16.
+//  Copyright © 2016 ChristianFruhauf. All rights reserved.
+//
+
+
+
+import UIKit
+
+import AVFoundation
+
+
+
+class CameraView: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    
+    // Setting up the variables for the camera
+    
+    var captureSession : AVCaptureSession?
+    
+    var stillImageOutput : AVCaptureStillImageOutput?
+    
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    
+    
+    
+    
+    
+    @IBOutlet var cameraView: UIView!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        
+        
+        
+        
+        //captureSession.decreaseSize(AVEdgeWidths.bounds)
+        
+        
+        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        previewLayer?.frame = cameraView.bounds
+        
+    }
+    
+    
+    
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        
+        // Capture session size
+        
+        captureSession?.sessionPreset = AVCaptureSessionPreset352x288
+        
+        
+        
+        
+        
+        
+        let backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        
+        
+        var error : NSError?
+        
+        var input: AVCaptureDeviceInput!
+        
+        do {
+            
+            input = try AVCaptureDeviceInput(device: backCamera)
+            
+        } catch let error1 as NSError {
+            
+            error = error1
+            
+            input = nil
+            
+        }
+        
+        
+        
+        if (error == nil && captureSession?.canAddInput(input) != nil) {
+            
+            
+            
+            captureSession?.addInput(input)
+            
+            stillImageOutput = AVCaptureStillImageOutput()
+            
+            stillImageOutput?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+            
+            
+            
+            if (captureSession?.canAddOutput(stillImageOutput) != nil) {
+                
+                captureSession?.addOutput(stillImageOutput)
+                
+                // Setting up the camera stream
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                
+                // Camera orientation
+                
+                previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+                
+                previewLayer?.frame = self.cameraView.layer.frame
+                
+                previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+                
+                cameraView.layer.addSublayer(previewLayer!)
+                
+                captureSession?.startRunning()
+                
+            }
+            
+            
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    @IBOutlet var tempImageView: UIImageView!
+    
+    /*  This function determines whether or not the user
+    tapped on the screen to take a photo */
+    
+    func didPressToTakePhoto() {
+        
+        
+        
+        if let videoConnection = stillImageOutput?.connectionWithMediaType(AVMediaTypeVideo) {
+            
+            videoConnection.videoOrientation = AVCaptureVideoOrientation.Portrait
+            
+            stillImageOutput?.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {(samplebuffer, error) in
+                
+                
+                
+                if samplebuffer != nil {
+                    
+                    
+                    
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(samplebuffer)
+                    
+                    let dataProvider = CGDataProviderCreateWithCFData(imageData)
+                    
+                    let cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, CGColorRenderingIntent.RenderingIntentDefault)
+                    
+                    let image = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.Right)
+                    
+                    
+                    
+                    self.tempImageView.image = image
+                    
+                    self.tempImageView.hidden = false
+                    
+                    UIImageWriteToSavedPhotosAlbum(image, self, nil, nil)
+                    
+                    
+                }
+                
+                
+                
+            })
+            
+        }
+        
+        
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        didPressToTakeAnother()
+        
+    }
+    
+    
+    var didTakePhoto = Bool()
+    
+    
+    
+    // This eventually leads back to the camera
+    
+    func didPressToTakeAnother() {
+        
+        if  didTakePhoto == true {
+            
+            tempImageView.hidden = true
+            
+            didTakePhoto = false
+            
+        }   else   {
+            
+            captureSession?.startRunning()
+            
+            didTakePhoto = true
+            
+            didPressToTakePhoto()
+            
+        }
+    }
+    
+    
+}
---
+++ Nutritious/UploadView.swift
@@ -0,0 +1,98 @@
+//
+//  SecondViewController.swift
+//  Nutritious
+//
+//  Created by Christian Frühauf on 11/03/16.
+//  Copyright © 2016 ChristianFruhauf. All rights reserved.
+//
+
+import UIKit
+
+class UploadView: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    
+    
+    @IBOutlet weak var previewImage: UIImageView!
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet var keyboardHeightLayoutConstraint: NSLayoutConstraint?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardNotification:", name: UIKeyboardWillChangeFrameNotification, object: nil)
+        
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func keyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let endFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.CGRectValue()
+            let duration:NSTimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+            let animationCurveRawNSN = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber
+            let animationCurveRaw = animationCurveRawNSN?.unsignedLongValue ?? UIViewAnimationOptions.CurveEaseInOut.rawValue
+            let animationCurve:UIViewAnimationOptions = UIViewAnimationOptions(rawValue: animationCurveRaw)
+            if endFrame?.origin.y >= UIScreen.mainScreen().bounds.size.height {
+                self.keyboardHeightLayoutConstraint?.constant = 0.0
+            } else {
+                self.keyboardHeightLayoutConstraint?.constant = endFrame?.size.height ?? 0.0
+            }
+            UIView.animateWithDuration(duration,
+                delay: NSTimeInterval(0),
+                options: animationCurve,
+                animations: { self.view.layoutIfNeeded() },
+                completion: nil)
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    @IBAction func chooseImageTapped(sender: AnyObject) {
+        
+        
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .PhotoLibrary
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypesForSourceType(.PhotoLibrary)!
+        imagePicker.allowsEditing = false
+        
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [NSObject : AnyObject]!) {
+        
+        self.previewImage.image = image
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
